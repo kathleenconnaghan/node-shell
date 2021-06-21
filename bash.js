e588a2e981f57c2d3972a1d8914faf19fa4038ef
caitlin-rich <caitlinboagrich@gmail.com>
2021-06-21 16:16:22 -0400

    stash

--- bash.js
+++ bash.js
@@ -3,7 +3,9 @@ process.stdin.write('prompt > ');
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
     const pwd = require('./pwd')
+    const ls = require('./ls')
     if (cmd === 'pwd'){pwd()}
+    if (cmd === 'ls'){ls()}
 
     process.stdout.write('You typed: ' + cmd);
     process.stdout.write('\nprompt > ');
