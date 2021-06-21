c93a1c1ed40d25aec35644f2e1565afee9723b3c
caitlin-rich <caitlinboagrich@gmail.com>
2021-06-21 15:01:36 -0400

    feat bash.js module

--- bash.js
+++ bash.js
@@ -1,8 +1,12 @@
-process.stdin.write('prompt >');
+process.stdin.write('prompt > ');
 
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
+    const pwd = require('./pwd')
+    if (cmd === 'pwd'){pwd()}
 
     process.stdout.write('You typed: ' + cmd);
     process.stdout.write('\nprompt > ');
 });
+
+
