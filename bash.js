1ff6088290cb18f5bf9465230c28daac1f0e730d
kathleenconnaghan <77138134+kathleenconnaghan@users.noreply.github.com>
2021-06-21 12:56:59 -0400

    finished stream communication

---
+++ bash.js
@@ -0,0 +1,8 @@
+process.stdin.write('prompt >');
+
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
+
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+});
